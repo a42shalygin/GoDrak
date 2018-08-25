ff2734fa69f6bf6dab6fa54a9f3340d48a028eed
a42shalygin <alex_shalygin@yahoo.com>
2018-08-25 23:00:49 +0300

    two sources done. Interpolation and sampler to go

--- Tools/Amination_converter.py
+++ Tools/Amination_converter.py
@@ -7,6 +7,7 @@ class RynnConverter(object):
 
     bones = None
     animations = None
+    name = None
 
     def __init__(self):
         self.bones = []
@@ -45,7 +46,8 @@ class RynnConverter(object):
         self.bones.append('Lsheath')
 
 
-    def get_animations(self, path_to_file):
+    def get_animations(self, path_to_file, name):
+        self.name = name
 
         animations = []
         if not os.path.isfile(path_to_file):
@@ -64,9 +66,10 @@ class RynnConverter(object):
                     data = re.findall('(-?\\d+\\.\\d+)', line)
                     if data and current_bone:
                         current_bone.timestamps.append(data[0])
-                        current_bone.animations.append(data[1:] + [0, 0, 0, 1])
+                        an = data[1:] + ['0', '0', '0', '1']
+                        current_bone.animations.append(an)
 
-        return animations
+        self.animations = animations
 
 
 
@@ -105,8 +108,73 @@ class RynnConverter(object):
 #         self.assertEquals(len(self.Rynn.bones), 33)
 #
 
+def indent(elem, level=0):
+    i = "\n" + level*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
+
 if __name__ == '__main__':
     conv = RynnConverter()
-    animations = conv.get_animations(r'C:\Games\Drakan_ed\Psygnosis\Drakan\Drakan Dump\Common\System'
-                                 r'\System [root]\Animations\Anim443 1handedRH.txt')
-    pass
+    conv.get_animations(r'C:\Games\Drakan_ed\Psygnosis\Drakan\Drakan Dump\Common\System'
+                                 r'\System [root]\Animations\Anim443 1handedRH.txt', 'Anim443_1handedRH')
+    with open('tmp', 'w') as out_file:
+        import xml.etree.ElementTree as ET
+        for anim in conv.animations:
+            fullname = conv.name + '_' + anim.name
+            animation_tag = ET.Element('animation')
+            animation_tag.set('id', fullname)
+            source_input = ET.SubElement(animation_tag, 'source')
+            source_input.set('id', fullname + '-input')
+            source_output = ET.SubElement(animation_tag, 'source')
+            source_output.set('id', fullname + '-output')
+            source_interpolation = ET.SubElement(animation_tag, 'source')
+            source_interpolation.set('id', fullname + '-interpolation')
+            sampler = ET.SubElement(animation_tag, 'source')
+            sampler.set('id', fullname + '-sampler')
+            channel = ET.SubElement(animation_tag, 'source')
+            channel.set('source', '#' + sampler.get('id'))
+            channel.set('target', 'Armature_%s/transform' % anim.name)
+
+            input_array = ET.SubElement(source_input, 'float array')
+            input_array.set('id', fullname + '-input-array')
+            input_array.set('count', str(len(anim.timestamps)))
+            input_array.text = '\n' + '\n'.join(anim.timestamps) + '\n'
+
+            input_technique_common = ET.SubElement(source_input, 'technique common')
+            input_accessor = ET.SubElement(input_technique_common, 'accessor')
+            input_accessor.set('source', '#' + input_array.get('id'))
+            input_accessor.set('count', str(len(anim.timestamps)))
+            input_accessor.set('stride', '1')
+
+            output_array = ET.SubElement(source_output, 'float array')
+            output_array.set('id', fullname + '-output-array')
+            output_array.set('count', str(len(anim.timestamps)*16))
+            output_array.text = '\n'
+            for an in anim.animations:
+                output_array.text += ' '.join(an) + '\n'
+
+            output_technique_common = ET.SubElement(source_output, 'technique common')
+            output_accessor = ET.SubElement(input_technique_common, 'accessor')
+            output_accessor.set('source', '#' + output_array.get('id'))
+            output_accessor.set('count', str(len(anim.timestamps) * 16))
+            output_accessor.set('stride', '16')
+
+            output_accessor_param = ET.SubElement(output_accessor, 'param')
+            output_accessor_param.set('name', 'TRANSFORM')
+            output_accessor_param.set('type', 'float4x4')
+
+            indent(animation_tag)
+            out_file.write(ET.tostring(animation_tag))
+
+
+
