556905adc0862177e9aa1b3da315012ad030f983
a42shalygin <alex_shalygin@yahoo.com>
2018-09-24 17:48:26 +0300

    Alt to hide mesh AnimatedColumn

--- Scenes/Skeleton.gd
+++ Scenes/Skeleton.gd
@@ -3,11 +3,21 @@ extends Skeleton
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var timer = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
     physical_bones_start_simulation()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	
+	if Input.is_key_pressed(KEY_ALT):
+		timer += delta
+		
+	if timer >= 0.1:
+		if not $pCube1.visible:
+			$pCube1.visible = true
+		else:
+			$pCube1.visible = false
+		
+		timer = 0
