a518aa56c755880cc86f8c83b6bb55f790df2ac3
a42shalygin <alex_shalygin@yahoo.com>
2018-11-04 17:39:45 +0300

    More fun with stuff

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,7 +3,7 @@ extends KinematicBody
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-var speed = 50
+var speed = 100
 var gravity = 10
 var cameraDirection
 
@@ -27,6 +27,9 @@ var doubleJumpDone = false
 
 var shield
 var weapon
+var weaponScene
+var weaponNodeName
+export var controlled = false
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -40,147 +43,149 @@ func _ready():
 	shield = load("res://Scenes/World/shield.tscn").instance()
 	$LH_slot.add_child(shield)
 	
-	weapon = load("res://Scenes/Weapon/Stick.tscn").instance()
-	$RH_slot.add_child(weapon)
+	weaponScene = load("res://Scenes/Weapon/Stick.tscn").instance()
+	$RH_slot.add_child(weaponScene)
+	weaponNodeName = weaponScene.name
+	weapon = get_node("RH_slot/" + weaponNodeName)
+	
+	
 	
 func _process(delta):
 	var direction = Vector3(0,0,0)
-	
-	
-	if Input.is_key_pressed(KEY_Z):
-		mouseCaptureTimer += delta
-	else:
-		mouseCaptureTimer = 0
-		
-	if mouseCaptureTimer >= 0.1:
-		if not mouseCapture:
-			mouseCapture = true
-			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+	# Moving weapon
+#	if weapon.get_translation().length() > 2:
+#		weapon.set_translation(Vector3(0,0,0))
+#	elif weapon.get_translation().length() < 1:
+#		weapon.move_and_slide(Vector3(0,0,0), Vector3(0,1,0))
+#	else:
+#		weapon.move_and_slide(-100 * get_translation(), Vector3(0,1,0))
+
+	if controlled:	
+		if Input.is_key_pressed(KEY_Z):
+			mouseCaptureTimer += delta
 		else:
-			mouseCapture = false
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		mouseCaptureTimer = 0
-	
-	freelook = Input.is_key_pressed(KEY_ALT)
-
-	# vector from camera to player
-	
-	var cameraOrigin = cameraNode.get_global_transform().origin
-	var playerOrigin = get_global_transform().origin
-	cameraDirection =  playerOrigin - cameraOrigin
-
-	# ===================== movement =====================
-	if Input.is_key_pressed(KEY_W):
-		direction += Vector3(cameraDirection.x, cameraDirection.y, cameraDirection.z)
-	if Input.is_key_pressed(KEY_S):
-		direction -= Vector3(cameraDirection.x, cameraDirection.y, cameraDirection.z)
-	if Input.is_key_pressed(KEY_A):
-		direction += Vector3(cameraDirection.z, 0, -cameraDirection.x)
-	if Input.is_key_pressed(KEY_D):
-		direction += Vector3(-cameraDirection.z, 0, cameraDirection.x)
-		
-	if Input.is_key_pressed(KEY_SHIFT):
-		run = 5
-	else:
-		run = 1
-	
-	direction = direction.normalized() * delta * speed * run
-	
-	
-	# Kinda gravity	
-	velocity.y -= gravity * delta
-	velocity.x = direction.x
-	velocity.z = direction.z
-	
-
-	# Jump 
-	if Input.is_key_pressed(KEY_SPACE):
-		if is_on_floor():
-			doubleJumpDone = false
-			velocity.y = +5
-		else:
-			if not doubleJumpDone:
-				if velocity.y < 0:
-					velocity.y +=5
-					doubleJumpDone = true
-					
-	# Move
-	velocity = move_and_slide(velocity, Vector3(0,1,0))
-	# ========================= CAMERA ==========================
-	
-	if mouseCapture:
-		get_viewport().warp_mouse(initMouse)
-		lastMouse = get_viewport().get_mouse_position()
-		
-		if abs(lastMouse.x - initMouse.x) > 2:
-			if freelook:
-				yawNode.rotate_y(-(lastMouse.x - initMouse.x) * mousespeed)	
-			else:
-				# TODO: SET YAW BACK TO CameraDirection
-				rotate_y(-(lastMouse.x - initMouse.x) * mousespeed)		
-		if abs(lastMouse.y - initMouse.y) > 2:
-			pitchNode.rotate_x(-(lastMouse.y - initMouse.y) * mousespeed)
-
-		
-		
-		# DEBUG
-#		if Input.is_key_pressed(KEY_RIGHT):
-#			yawNode.rotate_y(0.1)
-#		if Input.is_key_pressed(KEY_LEFT):
-#			yawNode.rotate_y(-0.1)
-#		if Input.is_key_pressed(KEY_UP):
-#			pitchNode.rotate_x(0.1)
-#		if Input.is_key_pressed(KEY_DOWN):
-#			pitchNode.rotate_x(-0.1)
-#
-	
-		
-		
-		
-		# Camera translation
-		if Input.is_mouse_button_pressed(BUTTON_RIGHT) and gimbalNode.scale > minScale:
-			# zoom in
-			gimbalNode.scale = gimbalNode.scale * Vector3(0.95, 0.95, 0.95)
-	    # Wheel Down Event
-		elif Input.is_mouse_button_pressed(BUTTON_LEFT) and gimbalNode.scale < maxScale:
-			# zoom out
-			gimbalNode.scale = gimbalNode.scale * Vector3(1.05, 1.05, 1.05)
-	
-	# ========================= ROLL ==============================
-	# Roll up to maxRoll in both cameraDirectionections
-	var current = rollNode.get_transform().basis.x.angle_to(Vector3(1,0,0))
-#	print(current)
-	if Input.is_key_pressed(KEY_Q) and current < maxRoll:
-		rollNode.rotate_z(0.01)
-	if Input.is_key_pressed(KEY_E) and current < maxRoll:
-		rollNode.rotate_z(-0.01)
-	
-	# Duplicating  node to rotate it right and check the cutrrent cameraDirectionection
-	var dup = rollNode.duplicate()
-	dup.rotate_z(0.0001)
-	var dupRoll = dup.get_transform().basis.x.angle_to(Vector3(1,0,0))
-	var isRolledRight
-	
-#	print('current = ' + str(current))
-#	print('dupRoll = ' + str(dupRoll))
-#	print(current - dupRoll)
-	if current > dupRoll:
-		isRolledRight = true
-	else:
-		isRolledRight = false
-	dup.rotate_z(-0.0001)
-	dup.free()
-	
-	if abs(current - 0) > 0.001:
-		if isRolledRight == true and not Input.is_key_pressed(KEY_E):
-			rollNode.rotate_z(0.005)
-		elif isRolledRight == false and not Input.is_key_pressed(KEY_Q):
-			rollNode.rotate_z(-0.005)
+			mouseCaptureTimer = 0
 			
+		if mouseCaptureTimer >= 0.1:
+			if not mouseCapture:
+				mouseCapture = true
+				Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+			else:
+				mouseCapture = false
+				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			mouseCaptureTimer = 0
+		
+		freelook = Input.is_key_pressed(KEY_ALT)
 	
-#	$Stick/Body.move_and_collide(Vector3(0,0,0))
+		# vector from camera to player
+		
+		var cameraOrigin = cameraNode.get_global_transform().origin
+		var playerOrigin = get_global_transform().origin
+		cameraDirection =  playerOrigin - cameraOrigin
+	
+		# ===================== movement =====================
+		if Input.is_key_pressed(KEY_W):
+			direction += Vector3(cameraDirection.x, cameraDirection.y, cameraDirection.z)
+		if Input.is_key_pressed(KEY_S):
+			direction -= Vector3(cameraDirection.x, cameraDirection.y, cameraDirection.z)
+		if Input.is_key_pressed(KEY_A):
+			direction += Vector3(cameraDirection.z, 0, -cameraDirection.x)
+		if Input.is_key_pressed(KEY_D):
+			direction += Vector3(-cameraDirection.z, 0, cameraDirection.x)
+			
+		if Input.is_key_pressed(KEY_SHIFT):
+			run = 5
+		else:
+			run = 1
+		
+		direction = direction.normalized() * delta * speed * run
+		
+		
+		# Kinda gravity	
+		velocity.y -= gravity * delta
+		velocity.x = direction.x
+		velocity.z = direction.z
+		
+	
+		# Jump 
+		if Input.is_key_pressed(KEY_SPACE):
+			if is_on_floor():
+				doubleJumpDone = false
+				velocity.y = +5
+			else:
+				if not doubleJumpDone:
+					if velocity.y < 0:
+						velocity.y +=5
+						doubleJumpDone = true
+						
+		# Move
+		velocity = move_and_slide(velocity, Vector3(0,1,0))
+		# ========================= CAMERA ==========================
+		
+		if mouseCapture:
+			get_viewport().warp_mouse(initMouse)
+			lastMouse = get_viewport().get_mouse_position()
+			
+			if abs(lastMouse.x - initMouse.x) > 2:
+				if freelook:
+					yawNode.rotate_y(-(lastMouse.x - initMouse.x) * mousespeed)	
+				else:
+					# TODO: SET YAW BACK TO CameraDirection
+					rotate_y(-(lastMouse.x - initMouse.x) * mousespeed)		
+			if abs(lastMouse.y - initMouse.y) > 2:
+				pitchNode.rotate_x(-(lastMouse.y - initMouse.y) * mousespeed)
+	
+			
+			# Camera translation
+			if Input.is_mouse_button_pressed(BUTTON_RIGHT) and gimbalNode.scale > minScale:
+				# zoom in
+				gimbalNode.scale = gimbalNode.scale * Vector3(0.95, 0.95, 0.95)
+		    # Wheel Down Event
+			elif Input.is_mouse_button_pressed(BUTTON_LEFT) and gimbalNode.scale < maxScale:
+				# zoom out
+				gimbalNode.scale = gimbalNode.scale * Vector3(1.05, 1.05, 1.05)
+		
+		# ========================= ROLL ==============================
+		# Roll up to maxRoll in both cameraDirectionections
+		var current = rollNode.get_transform().basis.x.angle_to(Vector3(1,0,0))
+	#	print(current)
+		if Input.is_key_pressed(KEY_Q) and current < maxRoll:
+			rollNode.rotate_z(0.01)
+		if Input.is_key_pressed(KEY_E) and current < maxRoll:
+			rollNode.rotate_z(-0.01)
+		
+		# Duplicating  node to rotate it right and check the cutrrent cameraDirectionection
+		var dup = rollNode.duplicate()
+		dup.rotate_z(0.0001)
+		var dupRoll = dup.get_transform().basis.x.angle_to(Vector3(1,0,0))
+		var isRolledRight
+		
+	#	print('current = ' + str(current))
+	#	print('dupRoll = ' + str(dupRoll))
+	#	print(current - dupRoll)
+		if current > dupRoll:
+			isRolledRight = true
+		else:
+			isRolledRight = false
+		dup.rotate_z(-0.0001)
+		dup.free()
+		
+		if abs(current - 0) > 0.001:
+			if isRolledRight == true and not Input.is_key_pressed(KEY_E):
+				rollNode.rotate_z(0.005)
+			elif isRolledRight == false and not Input.is_key_pressed(KEY_Q):
+				rollNode.rotate_z(-0.005)
+		
+		# ====================== IMPULSE =====================
+		if get_slide_count() > 0:
+			var collision = get_slide_collision(0)
+			if collision.collider is RigidBody:
+				collision.collider.apply_impulse(collision.position, -collision.normal / 10)
 
 
 func _on_Area_body_entered(body):
+	print(body.name + "entered!")
 	if body.is_in_group("Weapon"):
+		print("Weapon!")
 		self.queue_free()
